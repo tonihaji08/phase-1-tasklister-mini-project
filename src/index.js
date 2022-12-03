56c557b2197bb506eb26df140a761c36a0bae810
Your Name <antony.adriano@stundent.moringaschool.com>
2022-12-03 19:59:37 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
+// const { Toast } = require("bootstrap")
+
 document.addEventListener("DOMContentLoaded", () => {
+
   // your code here
-});
+ document.querySelector('form').addEventListener('submit',(e)=>{
+  e.preventDefault()
+  buildToDo(e.target.new_task.value)
+ })
+})
+function buildToDo(todo){
+  let p = document.createElement('p')
+  let btn = document.createElement('button')
+  btn.addEventListener('click',handleDelete)
+  btn.textContent = ' x'
+  p.textContent = `${todo}`
+  p.appendChild(btn)
+  document.querySelector('#list').appendChild(p)
+}
+function handleDelete(e){
+  e.target.parentNode.remove()
+
+}
+
